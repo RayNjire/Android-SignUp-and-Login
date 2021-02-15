3d401805a1067426dd10cb1a4e77184936e4b1a4
Ray Njire <ray.njire23@gmail.com>
2021-02-15 16:31:05 +0300

    Got Rid Of MySQL Connection To infinityfree.net. Remote Connection To DB Is Not Free
    Signed-off-by: Ray Njire <ray.njire23@gmail.com>

--- DataBaseConfig.php
+++ DataBaseConfig.php
@@ -8,25 +8,12 @@
         
         public function __construct()
         {
-            $this->servername = 'sql112.epizy.com';
-            $this->username = 'epiz_27906195';
-            $this->password = 'Df46BqALqFgzbAA';
-            $this->databasename = 'epiz_27906195_android_tidepredictions';
+            //freemysqlhosting.net
+            $this->servername = 'sql7.freemysqlhosting.net';
+            $this->username = 'sql7392265';
+            $this->password = 'Kfx2yziIGM';
+            $this->databasename = 'sql7392265';
             
-            
-            /** //infinityfree.com
-             * $this->servername = 'sql112.bytecluster.com'; or maybe 'sql112.epizy.com';
-             * $this->username = 'epiz_27906195';
-             * $this->password = 'Df46BqALqFgzbAA';
-             * $this->databasename = 'epiz_27906195_android_tidepredictions';
-             *
-             * //freemysqlhosting.net
-             * $this->servername = 'sql7.freemysqlhosting.net';
-             * $this->username = 'sql7392265';
-             * $this->password = 'Kfx2yziIGM';
-             * $this->databasename = 'sql7392265';
-             *
-            */
         }
     }
 
